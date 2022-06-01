eb417923d42e5d4d36401c62cc6ed24ec6620233
Kuttyhub <nishanth1216@gmail.com>
2022-06-01 15:20:13 +0530

    added checkout template

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,5 @@
+from distutils.log import error
+from itertools import product
 from django.shortcuts import render,redirect
 from django.template import loader
 from django.http import HttpResponse
@@ -50,4 +52,26 @@ def signup(request):
     })
 
 
+def checkout(req):
+    errors = []
+    if req.method == "POST":
+        name = req.POST['name']
+        email = req.POST['email']
+        address = req.POST['address']
+        city = req.POST['city']
+        pincode = req.POST['pincode']
 
+        product = req.POST['product']
+        quantity = req.POST['quantity']
+
+        if name and email and address and city and pincode and product and quantity:
+            # ins = Order(name=name, email=email, address=address, city=city, pincode=pincode, product=product, quantity=quantity)
+            # ins.save()
+            print("Data saved sucessfully")
+            return redirect("/")
+        else:
+            errors.append("Enter all the fields")
+            
+    return render(req,'checkout.html',{
+        'errors':errors,
+    })
